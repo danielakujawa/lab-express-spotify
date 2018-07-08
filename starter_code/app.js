962efa0e6f69c0165b3854bc158db27807fb86a2
danielakujawa <kujawa.daniela@gmail.com>
2018-07-08 17:24:32 +0200

    to be fixed

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,24 @@
+'use strict'
+
 const express = require('express');
 const app = express();
 const hbs = require('hbs');
+const bodyParser = require('body-parser');
+const morgan = require('morgan')
+const prettyjson = require('prettyjson');
 const path = require('path');
+
 const SpotifyWebApi = require('spotify-web-api-node');
+
+const clientId = '8d628f5b061f45409cdde686279f85a2';
+const clientSecret = '5a46fa3876d84df1be1a32cff68aaf8b';
 
 app.set('views', path.join(__dirname, 'views/layouts'));
 app.set('view engine', 'hbs');
-
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials');
 
-app.get('/', (req, res, next) => {
-  res.render('index');
-});
-
-// Remember to paste here your credentials
-const clientId = 'fc3c22905da0432899921c36d938e670',
-    clientSecret = '16bfc8e1a3734247be45adc7c0b546c4';
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const spotifyApi = new SpotifyWebApi({
   clientId : clientId,
@@ -30,5 +33,33 @@ spotifyApi.clientCredentialsGrant()
     console.log('Something went wrong when retrieving an access token', err);
 });
 
+app.get('/', (req, res, next) => {
+  res.render('index');
+});
 
-app.listen(3000);
+app.get('/artists', (req, res, next) => {
+  // console.log(req.query);
+
+  spotifyApi.searchArtists(req.query.artist)
+  .then((data) => {
+    console.log(data.body.artists.items[0].images)
+    res.render('artists', {artists: data.body.artists.items})
+  })
+  .catch(err => {
+    console.log(err);
+  })
+  ;
+
+});   
+
+app.get('/albums/:artistId', (req, res, next) => {
+
+  spotifyApi.getArtistAlbums(req.params.artistId)
+  .then(data => {
+    console.log(data);
+    res.render('albums', {albums: data.body.items})
+  })
+  
+});   
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
